c6190e0fa19a4de8422e6201ac9a6323c6ab1d5e
Helder Gabriel <125323362+helred1221@users.noreply.github.com>
2025-12-18 19:24:23 +0000

    add: adiciona scripts `lint:check` e `lint:write`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tabnews.com.br para aprender como criar um projeto do zero.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>O mundo é algo que pode ser extraordinário! Basta sermos o começo de tudo isso.</h1>
+  return (
+    <h1>
+      O mundo é algo que pode ser extraordinário! Basta sermos o começo de tudo
+      isso.
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("Teste2");
 }
 
 export default Home;
