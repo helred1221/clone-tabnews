463889602d3624c76993c3e9cac11ef1340cc4f7
Helder Gabriel <125323362+helred1221@users.noreply.github.com>
2026-01-09 01:49:32 +0000

    edit: mudei mensagem para "Em Construção...".

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
 function Home() {
   return (
     <h1>
-      O mundo é algo que pode ser extraordinário! Basta sermos o começo de tudo
-      isso.
+      Em construção...
     </h1>
   );
-}
-
-function teste() {
-  console.log("Teste");
-}
-
-function teste2() {
-  console.log("Teste2");
 }
 
 export default Home;
